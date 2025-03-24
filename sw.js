0ed6f8f5f7192c827a90693cda77ef06c3190cf1
msiraga <mounirsiraji@gmail.com>
2025-03-24 17:57:56 +0100

    V3- new style more modern

--- sw.js
+++ sw.js
@@ -1,22 +1,71 @@
-const CACHE_NAME = 'richdale-image-compare-v1';
+const CACHE_NAME = 'richdale-image-compare-v3';
 const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
   './icon192.png',
-  './icon512.png'
+  './icon512.png',
+  './icon.svg'
 ];
 
+// Install event
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
+  // Activate new service worker immediately
+  self.skipWaiting();
 });
 
+// Activate event
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            // Delete old caches
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => {
+      // Take control of all clients immediately
+      return clients.claim();
+    })
+  );
+});
+
+// Fetch event
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(response => {
+          // Check if we received a valid response
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+          // Clone the response
+          const responseToCache = response.clone();
+          caches.open(CACHE_NAME)
+            .then(cache => {
+              cache.put(event.request, responseToCache);
+            });
+          return response;
+        });
+      })
   );
+});
+
+// Message event - handle update notifications
+self.addEventListener('message', event => {
+  if (event.data === 'skipWaiting') {
+    self.skipWaiting();
+  }
 });
